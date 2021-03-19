6fd30b0624b9075cc919cd121c731858d2bcae0a
rongyi <rongyi@onchain.com>
2021-03-19 19:12:18 +0800

    Update wasm test

--- wasmtest/run-wasm-tests.sh
+++ wasmtest/run-wasm-tests.sh
@@ -3,16 +3,16 @@ set -e
 set -x
 
 # install build tools
-if ! which clang-9 ; then
+# if ! which clang-9 ; then
   wget releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz > /dev/null 2>&1
   tar xf clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz > /dev/null 2>&1
   export PATH="$(pwd)/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04/bin":$PATH
-fi
+# fi
 
-if ! which rustup ; then
+# if ! which rustup ; then
   curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly
   source $HOME/.cargo/env
-fi
+# fi
 # change default to nightly
 rustup default nightly
 rustup target add wasm32-unknown-unknown
