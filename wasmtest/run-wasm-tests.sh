4fcdb7d7d34dd671b5ed40f4eba18ea7120d57d1
rongyi <rongyi@onchain.com>
2021-03-19 19:44:04 +0800

    Update config

--- wasmtest/run-wasm-tests.sh
+++ wasmtest/run-wasm-tests.sh
@@ -3,17 +3,17 @@ set -e
 set -x
 
 # install build tools
-# if ! which clang-9 ; then
-  wget releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz > /dev/null 2>&1
-  tar xf clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz > /dev/null 2>&1
-  export PATH="$(pwd)/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04/bin":$PATH
-# fi
+# ensure clang
 
-# if ! which rustup ; then
-  curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly
-  source $HOME/.cargo/env
-# fi
-# change default to nightly
+wget releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz > /dev/null
+tar xf clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz > /dev/null 2>&1
+export PATH="$(pwd)/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04/bin":$PATH
+
+# ensure rust
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly
+source $HOME/.cargo/env
+
+
 rustup default nightly
 rustup target add wasm32-unknown-unknown
 which ontio-wasm-build || cargo install --git=https://github.com/ontio/ontio-wasm-build
