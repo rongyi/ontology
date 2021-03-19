73006d47e6376b3a4216906c1ea661d6e68383a4
rongyi <rongyi@onchain.com>
2021-03-19 20:16:31 +0800

    Update script

--- .travis.deploy.bash
+++ .travis.deploy.bash
@@ -4,23 +4,23 @@ set -ex
 VERSION=$(git describe --always --tags --long)
 PLATFORM=""
 
-# if [[ ${RUNNER_OS} == 'Linux' ]]; then
-#   PLATFORM="linux"
-# elif [[ ${RUNNER_OS} == 'osx' ]]; then
-#   PLATFORM="darwin"
-# else
-#   PLATFORM="windows"
-#   exit 1
-# fi
+if [[ ${RUNNER_OS} == 'Linux' ]]; then
+  PLATFORM="linux"
+elif [[ ${RUNNER_OS} == 'macOS' ]]; then
+  PLATFORM="darwin"
+else
+  PLATFORM="windows"
+  exit 1
+fi
 
 
 
-arch_list=( "linux" "darwin" )
-for cur_arch in "${arch_list[@]}"
-do
-    echo "build arch: $cur_arch"
+# arch_list=( "linux" "darwin" )
+# for cur_arch in "${arch_list[@]}"
+# do
+#     echo "build arch: $cur_arch"
 
-    PLATFORM=$cur_arch
+#     PLATFORM=$cur_arch
     env GO111MODULE=on make ontology-${PLATFORM} tools-${PLATFORM}
     mkdir tool-${PLATFORM}
     cp ./tools/abi/* tool-${PLATFORM}
@@ -33,4 +33,4 @@ do
     echo "ontology-${PLATFORM}-amd64 |" $(md5sum ontology-${PLATFORM}-amd64|cut -d ' ' -f1)
     echo "tool-${PLATFORM}.zip |" $(md5sum tool-${PLATFORM}.zip|cut -d ' ' -f1)
 
-done
+# done
