5b9ac8f7c5bb56ad179bcce476df8596ef99589c
rongyi <rongyi@onchain.com>
2021-06-17 16:45:38 +0800

    Update script

--- .gha.gofmt.sh
+++ .gha.gofmt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # code from https://github.com/Seklfreak/Robyul2
-which goimports || cd /tmp/; go get -u -v golang.org/x/tools/cmd/goimports; cd ../
+which goimports || cd /tmp/; go get -u -v golang.org/x/tools/cmd/goimports; cd -
 
 unset dirs files
 dirs=$(go list -f {{.Dir}} ./... | grep -v /vendor/)
